8e1c9fffc270136a34ab05468390d4a33d451c95
artballesteros <58927442+artballesteros@users.noreply.github.com>
2022-08-31 21:39:44 -0700

    Richest Customer Wealth

---
+++ 1672/RichestCustomerWealth.java
@@ -0,0 +1,22 @@
+/*
+Key Idea:
+    Iterate through each subarray and keep track of sum.
+    Compare sum to curr largest sum and replace if larger.
+    
+    Time: O(n^2)
+    Space: O(1)
+*/
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int largest = 0;
+        for (int[] account : accounts) {
+            int currWealth = 0;
+            for (int money : account) {
+                currWealth += money;
+            }
+            if (currWealth > largest)
+                largest = currWealth;
+        }
+        return largest;
+    }
+}

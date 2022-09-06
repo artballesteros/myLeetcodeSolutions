06f5fdf1407fc1f352cf75b2cc717a9298b7c096
artballesteros <58927442+artballesteros@users.noreply.github.com>
2022-09-05 19:52:15 -0700

    Finished 1299: Replace Element With Greatest Element On Right Side

---
+++ 1299/ReplaceElementsWithGreatestElementOnRightSide.java
@@ -0,0 +1,21 @@
+/*
+Key Idea:
+    Start from the left of the array and keep track of largest value
+    up to that point. 
+    
+    Runtime: O(n)
+    Space: O(1)
+*/
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int max = -1;
+        for (int i = arr.length - 1; i >= 0; --i) {
+            int temp = arr[i];
+            arr[i] = max;
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return arr;
+    }
+}

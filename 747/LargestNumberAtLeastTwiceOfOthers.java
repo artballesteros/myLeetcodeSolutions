32592f2e49d18bfc4caa4717d341f1390f684dd8
artballesteros <58927442+artballesteros@users.noreply.github.com>
2022-09-24 12:15:59 -0700

    Update 747: added a new solution [java]

--- 747/LargestNumberAtLeastTwiceOfOthers.java
+++ 747/LargestNumberAtLeastTwiceOfOthers.java
@@ -1,13 +1,24 @@
 /*
 [easy]
 Key Idea:
-    Just find the largest value and then compare it with the rest of the values.
+    Solution 1:
+    Just find the largest value and then compare it with the rest of the array.
     Keep note of the index.
     
     Time: O(n) (2n; we loop twice through the array, once for max, twice for compares)
     Space: O(1)
+    
+    Solution 2:
+    You can just find the two largest values and compare them. You can do
+    this with two passes like the first solution does or you can do it with 
+    once pass through the array by using two variables that "slide" accross
+    the array.
+    
+    Time: O(n) (one pass!)
+    Space: O(1)
 */
-class Solution {
+
+class Solution1 {
     private static final int MIN_VALUE = -1;
     public int dominantIndex(int[] nums) {
         int largestValue = MIN_VALUE;
@@ -27,5 +38,21 @@ class Solution {
         }
         
         return index;
+    }
+}
+
+class Solution2 {
+    public int dominantIndex(int[] nums) {
+        int max = -1; int sMax = -1; int index = -1;
+        for (int i = 0; i < nums.length; ++i) {
+            if (max < nums[i]) {
+                sMax = max;
+                max = nums[i];
+                index = i;
+            } else if (sMax < nums[i]) {
+                sMax = nums[i];
+            }
+        }
+        return max >= 2*sMax ? index : -1;
     }
 }
